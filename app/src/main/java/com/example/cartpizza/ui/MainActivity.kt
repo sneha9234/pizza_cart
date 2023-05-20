f88d5e24d2209dd34ceddc94300d1d4b99250712
snehasrishti <sneha@tamasha.live>
2023-05-20 23:49:56 +0530

    added initial dependencies, UI, data binding and root graph

---
+++ app/src/main/java/com/example/cartpizza/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.cartpizza.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cartpizza.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: MainActivityBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
